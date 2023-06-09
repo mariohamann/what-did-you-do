40c48dc2b6b654ff59078352dfe5f7c9ea510763
Christoph Saile <christoph.saile@virtual-identity.com>
2023-06-09 16:02:04 +0200

    Christophsaile/issue16 (#34)
    * feat: 🎸 add map component
    
    * feat: 🎸 render geoData in maps component
    
    * feat: 🎸 add clustering wip
    
    * feat: 🎸 add createGeoJson function
    
    * refactor: 💡 resolve ts error and restructure code
    
    * feat: 🎸 add zoom functionallity
    
    * fix: 🐛 visible actions
    
    * feat: 🎸 add categories, wip

---
+++ resources/js/Components/Map.vue
@@ -0,0 +1,289 @@
+<script setup lang="ts">
+import { ActionData, ActionsJsonData, CategoryData } from "@/types/generated";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { onMounted, onUnmounted, ref } from "vue";
+import { router, usePage } from "@inertiajs/vue3";
+import Action from "@/Components/Action.vue";
+
+interface GeoJSON {
+    type: string;
+    features: {
+        type: string;
+        properties: {
+            id: number;
+            category: number;
+        };
+        geometry: {
+            type: string;
+            coordinates: number[];
+        };
+    }[];
+}
+
+// TODO: usePage or pass as prop???
+const props = defineProps<{
+    apiKey: string;
+    geoData: ActionsJsonData[];
+    actions: ActionData[];
+}>();
+
+const mapCanvas = ref<HTMLElement>();
+const visibleActions = ref<number[]>([]);
+// TODO: usePage or pass as prop???
+const categories = ref<CategoryData[]>(
+    usePage().props.categories as CategoryData[]
+);
+let map = ref<maplibregl.Map>();
+
+function initMap(): void {
+    map.value = new maplibregl.Map({
+        container: mapCanvas.value!,
+        attributionControl: false,
+        style: `https://tiles.locationiq.com/v3/light/vector.json?key=${props.apiKey}`, // stylesheet location
+        center: [14.95, 50.02], // starting position [lng, lat]
+        zoom: 3, // starting zoom
+    });
+    addNavigationControl(map.value);
+    addGeolocateControl(map.value);
+    addAutoCompleteControl(map.value);
+    addSourceAndLayers(map.value);
+    setActionsInView(map.value);
+}
+
+function addNavigationControl(map: maplibregl.Map): void {
+    map.addControl(
+        new maplibregl.NavigationControl({
+            showCompass: false,
+            showZoom: true,
+            visualizePitch: false,
+        }),
+        "top-right"
+    );
+}
+
+function addGeolocateControl(map: maplibregl.Map): void {
+    map.addControl(
+        new maplibregl.GeolocateControl({
+            positionOptions: undefined,
+            fitBoundsOptions: undefined,
+            trackUserLocation: true,
+            showAccuracyCircle: true,
+            showUserLocation: true,
+        }),
+        "bottom-right"
+    );
+}
+
+function addAutoCompleteControl(map: maplibregl.Map): void {
+    map.addControl(
+        // new MapbodxGeocder class is loaded via CDN in app.blade.php
+        // @ts-ignore
+        new MapboxGeocoder({
+            accessToken: props.apiKey,
+            mapboxgl: maplibregl,
+            limit: 5,
+            dedupe: 1,
+            flyTo: {
+                screenSpeed: 7,
+                speed: 4,
+            },
+        }),
+        "top-left"
+    );
+}
+
+function addSourceAndLayers(map: maplibregl.Map): void {
+    map.on("load", () => {
+        // Add a new source from our GeoJSON data and
+        // set the 'cluster' option to true. GL-JS will
+        // add the point_count property to your source data.
+        map.addSource("actions", {
+            type: "geojson",
+            data: createGeoJson(props.geoData),
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+        });
+
+        map.addLayer({
+            id: "clusters",
+            type: "circle",
+            source: "actions",
+            filter: ["has", "point_count"],
+            paint: {
+                // Use step expressions (https://maplibre.org/maplibre-style-spec/#expressions-step)
+                // with three steps to implement three types of circles:
+                //   * Blue, 20px circles when point count is less than 100
+                //   * Yellow, 30px circles when point count is between 100 and 750
+                //   * Pink, 40px circles when point count is greater than or equal to 750
+                "circle-color": [
+                    "step",
+                    ["get", "point_count"],
+                    "#51bbd6",
+                    100,
+                    "#f1f075",
+                    750,
+                    "#f28cb1",
+                ],
+                "circle-radius": [
+                    "step",
+                    ["get", "point_count"],
+                    20,
+                    100,
+                    30,
+                    750,
+                    40,
+                ],
+            },
+        });
+
+        map.addLayer({
+            id: "cluster-count",
+            type: "symbol",
+            source: "actions",
+            filter: ["has", "point_count"],
+            layout: {
+                "text-field": "{point_count_abbreviated}",
+                "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+                "text-size": 12,
+            },
+        });
+
+        map.addLayer({
+            id: "unclustered-point",
+            type: "symbol",
+            source: "actions",
+            filter: ["!", ["has", "point_count"]],
+            layout: {
+                // TODO: emojis are not working in text-field
+                "text-field": "emoji🐢",
+                "text-size": 24,
+                "text-anchor": "center",
+            },
+            paint: {
+                "text-color": "#000000",
+            },
+        });
+
+        // inspect a cluster on click
+        map.on("click", "clusters", (e) => {
+            var features = map.queryRenderedFeatures(e.point, {
+                layers: ["clusters"],
+            });
+            var clusterId = features[0].properties.cluster_id;
+            // TODO: fix types
+            // @ts-ignore
+            map.getSource("actions")?.getClusterExpansionZoom(
+                clusterId,
+                // @ts-ignore
+                (err, zoom) => {
+                    if (err) return;
+
+                    map.easeTo({
+                        // TODO: fix types
+                        // @ts-ignore
+                        center: features[0].geometry.coordinates,
+                        zoom: zoom,
+                    });
+                }
+            );
+        });
+
+        // when clicking an unclustered point, highlight the action
+        map.on("click", "unclustered-point", (e) => {
+            if (e.features) {
+                const ids: number[] = [e.features[0].properties?.id];
+                // TODO highlight the action
+            }
+        });
+
+        // when the map moves, update the visible actions
+        map.on("moveend", () => {
+            setActionsInView(map);
+        });
+
+        map.on("mouseenter", "clusters", () => {
+            map.getCanvas().style.cursor = "pointer";
+        });
+        map.on("mouseleave", "clusters", () => {
+            map.getCanvas().style.cursor = "";
+        });
+    });
+}
+
+function createGeoJson(geoData: ActionsJsonData[]): GeoJSON {
+    return {
+        type: "FeatureCollection",
+        features: geoData.map((action) => {
+            return {
+                type: "Feature",
+                properties: {
+                    id: action.id,
+                    category: action.ca,
+                },
+                geometry: {
+                    type: "Point",
+                    coordinates: [action.ln, action.la],
+                },
+            };
+        }),
+    };
+}
+
+function setActionsInView(map: maplibregl.Map): void {
+    const matches = getActionIdsInCurrentView(map);
+    if (hasActiveActionsChanged(visibleActions.value, matches)) {
+        visibleActions.value = matches;
+        getActions(matches);
+    }
+}
+
+function getActionIdsInCurrentView(map: maplibregl.Map): number[] {
+    const matches: number[] = [];
+    props.geoData.forEach((item) => {
+        const ll = new maplibregl.LngLat(item.ln, item.la);
+        if (map.getBounds().contains(ll)) {
+            matches.push(item.id);
+        }
+    });
+    return matches;
+}
+
+function hasActiveActionsChanged(
+    active: number[],
+    currentInView: number[]
+): boolean {
+    return JSON.stringify(active) !== JSON.stringify(currentInView);
+}
+
+function getActions(ids: number[]): void {
+    router.get(
+        "/index",
+        { ids: ids },
+        { replace: true, preserveState: true, preserveScroll: true }
+    );
+}
+
+onMounted(() => {
+    initMap();
+});
+
+onUnmounted(() => {
+    map.value?.remove();
+});
+</script>
+
+<template>
+    <div class="mx-auto mt-12 grid w-full grid-cols-2 grid-rows-1 gap-8">
+        <div ref="mapCanvas" class="aspect-square"></div>
+        <div>
+            <h2>actions</h2>
+            <Action
+                v-for="action in actions"
+                v-bind="action"
+                v-bind:key="action.id"
+            />
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Actions/Index.vue
+++ resources/js/Pages/Actions/Index.vue
@@ -4,6 +4,7 @@ import { Head } from "@inertiajs/vue3";
 import Action from "@/Components/Action.vue";
 import CreateAction from "@/Components/CreateAction.vue";
 import GetActions from "@/Components/GetActions.vue";
+import Map from "@/Components/Map.vue";
 import { ref } from "vue";
 
 // It should be possible to remove this import as soon as https://github.com/vuejs/core/issues/4294 is completely done in Vue 3.3.0, but currently it is still needed.
@@ -13,13 +14,17 @@ let props = defineProps<ActionIndexData>();
 
 // amount of fetched elements from actions_json_url
 let actionsFromJsonLength = ref(0);
+// fetched data from actions_json_url
+let geoJson = ref<ActionsJsonData[]>([]);
 
 // fetch data from actions_json_url and make a console log of the length of the json (array)
+// and set the geoJson ref to the fetched data
 fetch(props.actions_json_url)
     .then((response) => response.json())
-    .then(
-        (data: ActionsJsonData[]) => (actionsFromJsonLength.value = data.length)
-    );
+    .then((data: ActionsJsonData[]) => {
+        actionsFromJsonLength.value = data.length;
+        geoJson.value = data;
+    });
 </script>
 
 <template>
@@ -42,6 +47,14 @@ fetch(props.actions_json_url)
 
         <div class="mx-auto mt-12 max-w-7xl sm:px-6 lg:px-8">
             Fetched elements from actions_json_url: {{ actionsFromJsonLength }}
+        </div>
+
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <Map
+                api-key="pk.ed59a693277d463a0b1bda2317c16928"
+                :geo-data="geoJson"
+                :actions="actions"
+            ></Map>
         </div>
 
         <div class="py-12">
