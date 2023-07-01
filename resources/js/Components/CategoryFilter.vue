89e89896e6dc27e6907f64b61ea31e503c494924
Christoph Saile <christoph.saile@virtual-identity.com>
2023-07-01 23:18:04 +0200

    feat: 🎸 add autocomplete and categroy filter

---
+++ resources/js/Components/CategoryFilter.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+} from "@headlessui/vue";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/vue/solid";
+import { CategoryData } from "@/types/generated";
+
+const props = defineProps<{
+    categories: CategoryData[];
+}>();
+
+const selectedCategory = ref(props.categories[0]);
+</script>
+
+<template>
+    <Listbox v-model="selectedCategory">
+        <div class="relative mt-1">
+            <ListboxButton
+                class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            >
+                <span class="block truncate">{{ selectedCategory.name }}</span>
+                <span
+                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+                >
+                    <ChevronDownIcon
+                        class="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                    />
+                </span>
+            </ListboxButton>
+
+            <transition
+                leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+            >
+                <ListboxOptions
+                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
+                    <ListboxOption
+                        v-slot="{ active, selected }"
+                        v-for="category in props.categories"
+                        :key="category.name"
+                        :value="category"
+                        as="template"
+                    >
+                        <li
+                            :class="[
+                                active
+                                    ? 'bg-amber-100 text-amber-900'
+                                    : 'text-gray-900',
+                                'relative cursor-default select-none py-2 pl-10 pr-4',
+                            ]"
+                        >
+                            <span
+                                :class="[
+                                    selected ? 'font-medium' : 'font-normal',
+                                    'block truncate',
+                                ]"
+                                >{{ category.name }}</span
+                            >
+                            <span
+                                v-if="selected"
+                                class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                            >
+                                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                            </span>
+                        </li>
+                    </ListboxOption>
+                </ListboxOptions>
+            </transition>
+        </div>
+    </Listbox>
+</template>
--- resources/js/Pages/Actions/Index.vue
+++ resources/js/Pages/Actions/Index.vue
@@ -2,7 +2,6 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { usePage, Head, useForm } from "@inertiajs/vue3";
 import Action from "@/Components/Action.vue";
-import CreateAction from "@/Components/CreateAction.vue";
 import Map from "@/Components/Map.vue";
 import { ref } from "vue";
 
@@ -49,66 +48,6 @@ fetch(props.actions_json_url)
                         api-key="pk.ed59a693277d463a0b1bda2317c16928"
                         :geo-data="geoJson"
                     ></Map>
-                    <div
-                        class="pointer-events-none fixed bottom-12 left-0 z-40 w-full xl:pl-96"
-                    >
-                        <div class="mx-auto flex justify-center">
-                            <form
-                                class="pointer-events-auto flex gap-3 rounded-[20px] border-black bg-secondary-300 p-3 shadow-sm shadow-secondary-100/10"
-                                action="#"
-                                method="GET"
-                            >
-                                <div class="">
-                                    <div class="relative rounded-md">
-                                        <label for="q" class="sr-only"
-                                            >Search value</label
-                                        >
-                                        <input
-                                            autofocus
-                                            type="text"
-                                            name="q"
-                                            id="q"
-                                            class="block w-full rounded-xl border-0 py-3 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-white placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-                                            placeholder="Search..."
-                                        />
-                                        <div
-                                            class="absolute inset-y-0 right-0 flex items-center"
-                                        >
-                                            <label for="mode" class="sr-only"
-                                                >Mode for Search</label
-                                            >
-                                            <select
-                                                id="mode"
-                                                name="mode"
-                                                class="h-full rounded-xl border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm"
-                                            >
-                                                <option>Location</option>
-                                                <option>Content</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <label for="category" class="sr-only"
-                                        >Mode for Search</label
-                                    >
-                                    <select
-                                        id="category"
-                                        name="category"
-                                        class="block w-full rounded-xl border-0 py-3 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-white focus:ring-2 focus:ring-primary-600 sm:text-sm sm:leading-6"
-                                    >
-                                        <option selected>All Categories</option>
-                                        <option
-                                            v-for="category in categories"
-                                            :key="category.id"
-                                        >
-                                            {{ category.name }}
-                                        </option>
-                                    </select>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
                 </div>
             </main>
 
